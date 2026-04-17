b088132fa6ac4736331302ec5ee459e1ffcffba3
Connor Loyles <conloyles@gmail.com>
2026-04-16 20:21:09 -0700

    Added k8s/ingress.yaml file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
 
 app = Flask(__name__)
+metrics = PrometheusMetrics(app)
 
 inventory = [
     {"id": 1, "sku": "NK-001", "brand": "Nike", "category": "athletic", "quantity": 42},
