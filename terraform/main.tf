24e669b5d24ae546d09e86a37d1abcf17c776936
Connor Loyles <conloyles@gmail.com>
2026-05-21 08:54:36 -0700

    feat: add iniial terraform config with s3 bucket and output

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.0"
+}
 
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "inventory_state" {
+    bucket = "connor-inventory-api-tf-state"
+
+    tags = {
+        Name            = "inventory_api_tf_state"
+        Environtment    = "dev" 
+        Project         = "inventory-api"
+    }
+}
+output "s3_bucket_name" {
+  value       = aws_s3_bucket.inventory_state.bucket
+  description = "The name of the S3 bucket"
+}
